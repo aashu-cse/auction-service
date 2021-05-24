f16bf18079a480810964fcc757568a7b7658152a
Ashish Anand <ashishanand@qainfotech.com>
2021-05-24 23:09:06 +0530

    initial commit

---
+++ src/handlers/getAuctionsById.js
@@ -0,0 +1,31 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+import commonMiddleware from "../lib/commonMiddleware";
+import createError from 'http-errors'
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function getAuctionsById(event, context) {
+  let auction;
+  const { id } = event.pathParameters
+  try {
+    const result = await dynamodb.get({
+      TableName: process.env.AUCTIONS_TABLE,
+      Key:{id}
+    }).promise();
+    auction = result.Item
+  } catch (error) {
+    console.log(error)
+    throw new createError.InternalServerError(error)
+  }
+
+  if(!auction){
+    throw new createError.NotFound('No record found');
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(auction),
+  };
+}
+
+export const handler = commonMiddleware(getAuctionsById)
